c26b72d53c79e439d170cd3c5cef2f8c400284de
Leesito <al.leedelicado@iesramonllull.net>
2023-10-27 12:52:35 +0200

    mi primer commit

---
+++ main.py
@@ -0,0 +1,13 @@
+print("Hola mundo, me llamo Josefino!!")
+print("Hola mundo 1!!")
+
+#Esto es un comentario
+print("Hola mundo 2!!")
+
+"""
+Texto que no se va a interpretar
+Texto que no se va a interpretar
+Texto que no se va a interpretar
+"""
+
+#Variables
